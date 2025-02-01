d2ec06d972ced18a6d69f6c8a7ae99de3bd8e98c
A-4-Atom <vcikas2626@gmail.com>
2025-02-01 20:40:48 +0530

    added sound and fixed input ref

---
+++ Number Guessing Game/src/assets/index.js
@@ -0,0 +1,5 @@
+import clickSound from "./click.mp3";
+import resetSound from "./reset.wav";
+import loseSound from "./lose.wav";
+import winSound from "./win.wav";
+export { clickSound, resetSound, loseSound, winSound };
